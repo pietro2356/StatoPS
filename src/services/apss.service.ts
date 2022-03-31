f7f257e7825fa64331668815ac2d9a5ef2d2f5de
pietro2356 <pietro.rocchio@gmail.com>
2022-03-31 14:06:10 +0200

    Add services & module

---
+++ src/services/apss.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApssService {
+
+  ApssApiUrl: string = "https://servizi.apss.tn.it/opendata/STATOPS001.json";
+
+  constructor(public http: HttpClient) { }
+
+  getStatoPS(): Observable<any>{
+    return this.http.get<any>(this.ApssApiUrl);
+  }
+}
